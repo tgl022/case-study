c6483d3cdfbe50d5107a55d38da6d9d6cd2ddab1
Tomas <tomas.g.larson@gmail.com>
2017-08-08 19:20:00 -0500

    heroku 2.2

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
 const express = require('express');
+const http = require('http');
+const path = require('path');
+
 const app = express();
 
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-       ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-}
+// const forceSSL = function() {
+//   return function (req, res, next) {
+//     if (req.headers['x-forwarded-proto'] !== 'https') {
+//       return res.redirect(
+//        ['https://', req.get('Host'), req.url].join('')
+//       );
+//     }
+//     next();
+//   }
+// }
+//
+// app.use(forceSSL());
 
-app.use(forceSSL());
-
-const path = require('path');
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-app.use(express.static(__dirname + '/dist'));
 
-app.listen(process.env.PORT || 8080);
+const port = process.env.PORT || 8080;
+app.set('port', port);
+app.listen(port, () => console.log('Running on ' + port));
